b529fc7728e09680117878734cf1780edaa0edf0
Daniel Sousa <daniel.sousa.developer@gmail.com>
2020-08-04 16:42:02 -0300

    Onboarding finished

--- src/screens/Onboarding/index.tsx
+++ src/screens/Onboarding/index.tsx
@@ -1,9 +1,16 @@
 import React, { useRef } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import Animated, { multiply } from 'react-native-reanimated';
+import { View, StyleSheet, Dimensions, Image } from 'react-native';
+import Animated, {
+  multiply,
+  divide,
+  Extrapolate,
+  interpolate,
+} from 'react-native-reanimated';
 import { useScrollHandler, interpolateColor } from 'react-native-redash';
 import Slide from '../../components/Slide';
 import Subslide from '../../components/Subslide';
+import Dot from '../../components/Dot';
+
 import { useNavigation } from '@react-navigation/native';
 
 interface Slide {
@@ -37,6 +44,28 @@ const Oboarding = () => {
       <Animated.View
         style={[styles.slider, { backgroundColor: backgroundColor }]}
       >
+        {slides.map(({ picture }, index) => {
+          const opacity = interpolate(x, {
+            inputRange: [
+              (index - 0.5) * width,
+              index * width,
+              (index + 0.5) * width,
+            ],
+            outputRange: [0, 1, 0],
+            extrapolate: Extrapolate.CLAMP,
+          });
+          return (
+            <Animated.View style={[styles.underlay, { opacity }]} key={index}>
+              <Image
+                source={picture.src}
+                style={{
+                  width: width - 75,
+                  height: ((width - 75) * picture.height) / picture.width,
+                }}
+              />
+            </Animated.View>
+          );
+        })}
         <Animated.ScrollView
           ref={scroll}
           horizontal
@@ -47,44 +76,49 @@ const Oboarding = () => {
           {...scrollHandler}
         >
           {slides.map(({ title, picture }, index) => (
-            <Slide key={index} right={!!(index % 2)} title={title} />
+            <Slide key={index} right={!!(index % 2)} {...{ title, picture }} />
           ))}
         </Animated.ScrollView>
       </Animated.View>
+
       <View style={styles.footer}>
         <Animated.View
           style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
-        ></Animated.View>
-        <Animated.View
-          style={[
-            styles.footerContent,
-            {
+        />
+        <View style={styles.footerContent}>
+          <View style={styles.pagination}>
+            {slides.map((_, index) => (
+              <Dot key={index} currentIndex={divide(x, width)} {...{ index }} />
+            ))}
+          </View>
+          <Animated.View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
               width: width * slides.length,
               transform: [{ translateX: multiply(x, -1) }],
-            },
-          ]}
-        >
-          {slides.map(({ subtitle, description }, index) => {
-            const last = index === slides.length - 1;
-            return (
-              <Subslide
-                key={index}
-                onPress={() => {
-                  if (last) {
-                    navigation.navigate('Welcome');
-                  } else {
-                    scroll.current
-                      ?.getNode()
-                      .scrollTo({ x: width * (index + 1), animated: true });
-                  }
-                }}
-                subtitle={subtitle}
-                description={description}
-                last={last}
-              />
-            );
-          })}
-        </Animated.View>
+            }}
+          >
+            {slides.map(({ subtitle, description }, index) => {
+              const last = index === slides.length - 1;
+              return (
+                <Subslide
+                  key={index}
+                  onPress={() => {
+                    if (last) {
+                      navigation.navigate('Welcome');
+                    } else {
+                      scroll.current
+                        ?.getNode()
+                        .scrollTo({ x: width * (index + 1), animated: true });
+                    }
+                  }}
+                  {...{ subtitle, description, last }}
+                />
+              );
+            })}
+          </Animated.View>
+        </View>
       </View>
     </View>
   );
@@ -95,6 +129,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
+  underlay: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    borderBottomRightRadius: 75,
+    overflow: 'hidden',
+  },
   slider: {
     height: 0.61 * height,
     borderBottomRightRadius: 75,
@@ -102,9 +143,15 @@ const styles = StyleSheet.create({
   footer: {
     flex: 1,
   },
+  pagination: {
+    ...StyleSheet.absoluteFillObject,
+    height: 75,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   footerContent: {
     flex: 1,
-    flexDirection: 'row',
     backgroundColor: 'white',
     borderTopLeftRadius: 75,
   },
